8481403850ccf49812038f1f97ef30b3e5344e4a
eddymandran <dev.eddy.mandran@gmail.com>
2020-10-13 21:46:00 +0200

    React 03 - Map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className='App'>
-      <Contact
-        avatar='https://randomuser.me/api/portraits/women/53.jpg'
-        name='Christy Cole'
-        online
-      />
-      <Contact
-        avatar='https://randomuser.me/api/portraits/men/45.jpg'
-        name='Jimmy Miles'
-      />
-      <Contact
-        avatar='https://randomuser.me/api/portraits/women/46.jpg'
-        name='Rhonda Green'
-        online
-      />
+      <ContactList />
     </div>
   );
 }
