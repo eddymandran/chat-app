0e91fafd2d608d58fe4ac509988e09e97953e5ae
eddymandran <dev.eddy.mandran@gmail.com>
2020-10-14 12:13:06 +0200

    props.online

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOnline: false,
+      isOnline: props.online,
     };
   }
   render() {
